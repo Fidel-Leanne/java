67dc586bb32c6b6b58bf55872a7f5e627d1e4a87
fidelia <fidelialtm13@gmail.com>
2024-02-18 17:45:04 +0200

    String Tokenizer

---
+++ StringTokenizerExample.java
@@ -0,0 +1,13 @@
+import java.util.StringTokenizer;
+
+public class StringTokenizerExample {
+    public static void main(String[] args) {
+        /* StringTokenizer object */
+        StringTokenizer st = new StringTokenizer("Demonstrating methods from StringTokenizer class ");
+
+        /* Checks if the String has any more tokens */
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+}
---
+++ practice.java
@@ -0,0 +1,11 @@
+public class practice {
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        int x = 7, y = 8;
+
+        System.out.println(y);
+
+        System.out.println();
+    }
+}
